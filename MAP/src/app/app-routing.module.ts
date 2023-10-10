e8f90fee45a9be3da875f88a19e4b251ca5e54f3
Caio <caiocaliman@gmail.com>
2023-10-10 12:37:25 -0300

    atualização das telas

---
+++ MAP/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './telas/login/login.component';
+import { CadastroComponent } from './telas/cadastro/cadastro.component';
+import { TelaInicialComponent } from './telas/tela-inicial/tela-inicial.component';
+import { MetodologiasComponent } from './telas/metodologias/metodologias.component';
+import { CriacaoMapComponent } from './telas/criacao-map/criacao-map.component';
+import { MapComponent } from './telas/map/map.component';
+
+const routes: Routes = [
+  { path: '', component: TelaInicialComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'metodologias', component: MetodologiasComponent},
+  { path: 'criacao-map', component: CriacaoMapComponent},
+  { path: 'map', component: MapComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
